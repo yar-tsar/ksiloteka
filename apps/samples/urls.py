5e1c1f37e41b42a2d7fed3b66c8b3ff5160836df
yar-tsar <yaraslau.tsar@gmail.com>
2020-01-30 02:29:01 +0300

    Added sample details page, changed CSS file.

--- apps/samples/urls.py
+++ apps/samples/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.samples_list, name='samples_list'),
+    path('sample/<int:pk>/', views.sample_detail, name='sample_detail'),
 ]
--- apps/samples/views.py
+++ apps/samples/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Sample
+from django.shortcuts import render, get_object_or_404
+
+def sample_detail(request, pk):
+    sample = get_object_or_404(Sample, pk=pk)
+    return render(request, 'samples/sample_detail.html', {'sample': sample})
 
 def samples_list(request):
     posts = Sample.objects.order_by('latin')
